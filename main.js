02eb40c87d7945c83e05e6146dfcc9d79af199cb
mnajafy <64306946+mnajafy@users.noreply.github.com>
2021-05-04 07:40:25 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function(){  
-        if ($(this).scrollTop() > 40) {
-            $('header').addClass("sticky");
-        }else{
-            $('header').removeClass("sticky");
-        }   
-    });
-});
